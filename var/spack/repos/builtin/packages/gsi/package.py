ecfb9f449d190e62931336e0e8745d58d5d0ff71
AlexanderRichert-NOAA <alexander.richert@noaa.gov>
2024-10-05 04:41:43 +0000

    add gsi recipe

---
+++ var/spack/repos/builtin/packages/gsi/package.py
@@ -0,0 +1,60 @@
+# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class Gsi(CMakePackage):
+    """Gridpoint Statistical Interpolation"""
+
+    homepage = "https://www.example.com"
+    url = "gsi"
+    git = "https://github.com/NOAA-EMC/GSI"
+
+    maintainers("AlexanderRichert-NOAA")
+
+    license("LGPL-3.0-only", checked_by="AlexanderRichert-NOAA")
+
+    version("develop", branch="develop", submodules=True)
+    version("rrfs-v1.0.0", branch="release/rrfs.v1.0.0", submodules=True)
+
+    variant("gsi", default=True)
+    variant("enkf", default=True, when="+gsi")
+    variant("gsdcloud", default=False)
+    variant("mgbf", default=True)
+    variant("gsi_mode", default="GFS", values=("GFS", "Regional"), when="+gsi")
+    variant("enkf_mode", default="GFS", values=("GFS", "WRF", "NMMB", "FV3REG"), when="+enkf")
+
+    depends_on("mpi")
+    depends_on("netcdf-fortran")
+    depends_on("bacio")
+    depends_on("sigio")
+    depends_on("sfcio")
+    depends_on("nemsio")
+    depends_on("ncio")
+    depends_on("sp")
+    depends_on("w3emc")
+    depends_on("lapack")
+    wrfio_enkf_modes = ("WRF", "NMMB", "FV3REG")
+    for mode in wrfio_enkf_modes:
+        depends_on("wrf-io", when=f"enkf_mode={mode}")
+    with when("+gsi"):
+        depends_on("gsi-ncdiag")
+        depends_on("ip")
+        depends_on("bufr@:11")
+        depends_on("crtm")
+        depends_on("wrf-io", when="gsi_mode=Regional")
+
+
+    def cmake_args(self):
+        using_mkl = self.spec.satisfies("^[virtuals=lapack] intel-oneapi-mkl")
+        args = [ 
+            self.define_from_variant("BUILD_GSI", "gsi"),
+            self.define_from_variant("BUILD_ENKF", "enkf"),
+            self.define_from_variant("BUILD_GSDCLOUD", "gsdcloud"),
+            self.define_from_variant("BUILD_MGBF", "mgbf"),
+            self.define("ENABLE_MKL", using_mkl)
+        ]
+        return args
