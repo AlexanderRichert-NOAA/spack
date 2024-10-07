84b0cd36201069d70ca6dfab8463f368c5c519e4
Alex Richert <alexander.richert@noaa.gov>
2024-10-07 15:05:49 -0700

    update gsi, incl. fix resource

--- var/spack/repos/builtin/packages/gsi/package.py
+++ var/spack/repos/builtin/packages/gsi/package.py
@@ -24,6 +24,12 @@ class Gsi(CMakePackage):
     variant("mgbf", default=True)
     variant("gsi_mode", default="GFS", values=("GFS", "Regional"), when="+gsi")
     variant("enkf_mode", default="GFS", values=("GFS", "WRF", "NMMB", "FV3REG"), when="+enkf")
+    variant("fix", default=False, description="Install fix files")
+
+    requires("+mgbf", when="+gsi")
+
+    depends_on("c", type="build")
+    depends_on("fortran", type="build")
 
     depends_on("mpi")
     depends_on("netcdf-fortran")
@@ -45,10 +51,19 @@ class Gsi(CMakePackage):
         depends_on("crtm")
         depends_on("wrf-io", when="gsi_mode=Regional")
 
+    resource(
+        name="gsifix",
+        url="https://ftp.emc.ncep.noaa.gov/static_files/public/gsi/gsi-fix-b90f7b.tgz",
+        sha256sum="b90f7b939482f1f0dd2700e2b108554bf527fe39a6f44d275545b1bf67fef09d",
+        placement="gsifix",
+        when="+fix",
+    )
+
 
     def setup_build_environment(self, env):
         if self.spec.satisfies("+fix"):
             env.set("GSI_BINARY_SOURCE_DIR", join_path(self.stage.source_path, "gsifix"))
+
 
     def cmake_args(self):
         using_mkl = self.spec.satisfies("^[virtuals=lapack] intel-oneapi-mkl")
@@ -56,9 +71,11 @@ class Gsi(CMakePackage):
             self.define_from_variant("BUILD_GSI", "gsi"),
             self.define_from_variant("BUILD_ENKF", "enkf"),
             self.define_from_variant("BUILD_GSDCLOUD", "gsdcloud"),
-            self.define_from_variant("BUILD_MGBF", "mgbf"),
-            self.define("ENABLE_MKL", using_mkl),
+            self.define_from_variant("USE_GSDCLOUD", "gsdcloud"),
+            self.define("BUILD_MGBF", "mgbf"),
+            self.define("USE_MGBF", "mgbf"),
             self.define("CMAKE_C_COMPILER", self.spec["mpi"].mpicc),
             self.define("CMAKE_Fortran_COMPILER", self.spec["mpi"].mpifc),
+            self.define("ENABLE_MKL", using_mkl),
         ]
         return args
